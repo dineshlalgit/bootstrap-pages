a1b2a074adf859ee74d09a91794b7719e0a9b1bd
dineshlal <dineshlal29@gmail.com>
2020-10-10 16:51:55 +0530

    BOOTSTRAP PAGES

---
+++ pagejs/ass1.js
@@ -0,0 +1,25 @@
+var lbs=prompt("Enter the pounds to convert into Kg");
+var kg=lbs*0.454;
+    alert("Kg is "+kg);
+    var n=prompt("Enter the number range")
+    console.log("ODD NUMBERS");
+    for(i=0;i<n;i++){
+        if(i%2!==0){
+            console.log(i);
+        }
+    }
+   alert("Project js basic 1:")
+   var firstname=prompt("Enter your firstname:");
+   var last=prompt("Enter your lastname:");   
+   var age=prompt("Enter your age:");
+   var tall=prompt("Enter your height in cms:");
+    console.log("Firstname:"+firstname);
+    console.log("lastname:"+last);
+    console.log("age"+age);
+    if(tall==170){
+        console.log(tall);    
+    }
+      else{
+        console.log("sorry condition not satified");
+      }
+      alert(firstname+" "+last+"thank you for ur information");
